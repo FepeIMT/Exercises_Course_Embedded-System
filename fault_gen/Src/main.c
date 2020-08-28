f76b279ab1c123138cf3e13b8c108de6b2350bb8
Fernando Aguilar <fernando.aguilar1010@gmail.com>
2020-08-27 21:28:41 -0500

    Completing the code where is analized the UsageFault Exception

--- fault_gen/Src/main.c
+++ fault_gen/Src/main.c
@@ -21,8 +21,13 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include <stdio.h>
+#include <stdint.h>
+
 int main(void)
 {
+
+	printf("Hello World\n");
 
 	//1.- enable all configurable exceptions like usage fault, mem manage fault and bus fault
 
@@ -33,6 +38,14 @@ int main(void)
 	*pSHCSR |= (1 << 18); // Usage fault enable bit
 
 	//3.- lets force the processor to execute some undefined instruction
+	uint32_t *pSRAM = (uint32_t*)0x20010000; /* This is a random memoru location */
+	*pSRAM = 0xFFFFFFFF;
+
+	void (*some_address) (void);
+
+	some_address = (void*)0x20010000;
+
+	some_address(); // Jump to pSRAM direction that contain 0xFFFFF
 
 
 	//4.- Analyze the fault
@@ -61,9 +74,33 @@ void BusFault_Handler(void)
 
 }
 
-void UsageFault_Handler(void)
+__attribute__ ((naked)) void UsageFault_Handler(void)
 {
+	//here we extracted the value of MSP which happens to be the
+	//base address of the stack frame(thread mode) which got saved during the expetion entry
+	//from thread mode to handler mode
+	__asm ("MRS r0, MSP");
+	__asm ("B UsageFault_Handler_c");
+}
+
+
+void UsageFault_Handler_c(uint32_t *pBaseStackFrame)
+{
+	uint32_t *pUFSR = (uint32_t*)0xE000ED2A;
 	printf("Exception : UsageFault\n");
+	printf("UFSR = %lx\n", (*pUFSR) & 0xFFFF);
+	printf("pBaseStackFrame = %p\n", pBaseStackFrame);
+	printf("Value of R0 = %lx\n", pBaseStackFrame[0]);
+	printf("Value of R1 = %lx\n", pBaseStackFrame[1]);
+	printf("Value of R2 = %lx\n", pBaseStackFrame[2]);
+	printf("Value of R3 = %lx\n", pBaseStackFrame[3]);
+	printf("Value of R12 = %lx\n", pBaseStackFrame[4]);
+	printf("Value of LR = %lx\n", pBaseStackFrame[5]);
+	printf("Value of PC = %lx\n", pBaseStackFrame[6]);
+	printf("Value of XPSR = %lx\n", pBaseStackFrame[7]);
+
+
+
 	while(1);
 }
 
